5d67d3f49634209d59a2f685be8fbba419c1ac4a
Ahmed <aelaimy@dot.jo>
2022-06-29 18:51:07 +0200

    first commit

---
+++ MobileTestApp/TasksCell.swift
@@ -0,0 +1,23 @@
+//
+//  TasksCell.swift
+//  MobileTestApp
+//
+//  Created by Ahmed El-elaimy on 29/06/2022.
+//
+
+import UIKit
+
+class TasksCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
